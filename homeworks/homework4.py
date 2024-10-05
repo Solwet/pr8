1ddad021197942525c4fb61bd2731302a546d06a
Solwet <rmuhtarov54@gmail.com>
2024-10-05 14:28:03 +0500

    itog

--- homeworks/homework4.py
+++ homeworks/homework4.py
@@ -1,13 +1,17 @@
+import math
 def num_input(prompt):
     while True:
         try:
-            return int(input(prompt))
+            return float(input(prompt)) 
         except ValueError:
-            print("Ошибка: пожалуйста, введите целое число.")
+            print("Ошибка: пожалуйста, введите число.")
 
 a = num_input("Введите первое число (a): ")
 b = num_input("Введите второе число (b): ")
 
+if a > b:
+    a, b = b, a
+
 print("Целые числа между", a, "и", b, ":")
-for i in range(a + 1, b):
+for i in range(math.ceil(a), math.floor(b)):
     print(i)
